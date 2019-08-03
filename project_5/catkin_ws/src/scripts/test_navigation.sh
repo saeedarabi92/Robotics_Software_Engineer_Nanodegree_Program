997d07400909055053c17a269cc54e00ebedc709
saeed <arabi@iastate.edu>
2019-08-02 19:34:19 -0500

    Update Project 5

---
+++ project_5/catkin_ws/src/scripts/test_navigation.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch " &
+sleep 2
+xterm  -e  " roslaunch turtlebot_rviz_launchers view_navigation.launch " &
+sleep 2
+xterm  -e  " roslaunch turtlebot_gazebo amcl_demo.launch" &
+sleep 2
+xterm  -e  " roslaunch turtlebot_teleop keyboard_teleop.launch"
--- project_5/launch.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-xterm  -e  " gazebo " &
-sleep 2
-xterm  -e  " source /opt/ros/kinetic/setup.bash; roscore" &
-sleep 2
-xterm  -e  " rosrun rviz rviz"
